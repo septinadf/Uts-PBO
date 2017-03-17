dbdabb7d76e80e76e8d0da385bc747695dcff571
User <septinarika90@gmail.com>
2017-03-17 20:44:46 +0700

    1

---
+++ src/tiket/kereta/septina/file_koneksi.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tiket.kereta.septina;
+import com.mysql.jdbc.Connection;
+import com.mysql.jdbc.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+/**
+ *
+ * @author User
+ */
+public class file_koneksi {
+    private static Connection koneksi;
+public static Connection GetConnection() throws SQLException {
+if (koneksi == null) {
+Driver driver = new Driver();
+koneksi = (Connection) DriverManager.getConnection("jdbc:mysql://localhost:3306/tiketkereta?zeroDateTimeBehavior=convertToNull", "root", "");
+}
+return koneksi;
+}
+    
+}
